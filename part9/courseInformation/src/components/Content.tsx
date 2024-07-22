a080374668fe803752c50a551a525a1889d0320d
arecouz <richardcouzens96@gmail.com>
2024-07-22 17:56:57 +0100

    (Vite and typeScript)

--- part9/courseInformation/src/components/Content.tsx
+++ part9/courseInformation/src/components/Content.tsx
@@ -2,14 +2,19 @@ import { Course } from '../types';
 
 const Content = ({ courseParts }: { courseParts: Course[] }) => {
   return (
-    <>
+    <div>
       {courseParts.map((course) => (
         <p key={course.name}>
           {course.name} {course.exerciseCount}{' '}
         </p>
       ))}
-    </>
+    </div>
   );
 };
 
 export default Content;
+
+
+// TODO: 
+
+// Exercise 9.15
--- part9/courseInformation/src/types.ts
+++ part9/courseInformation/src/types.ts
@@ -2,3 +2,24 @@ export interface Course {
   name: string;
   exerciseCount: number;
 }
+
+export interface CoursePartBasic extends Course {
+  description: string;
+  kind: 'basic';
+}
+
+export interface CoursePartGroup extends Course {
+  groupProjectCount: number;
+  kind: 'group';
+}
+
+export interface CoursePartBackground extends Course {
+  description: string;
+  backgroundMaterial: string;
+  kind: 'background';
+}
+
+export type CoursePart =
+  | CoursePartBasic
+  | CoursePartGroup
+  | CoursePartBackground;
