4a28d9b8431fbbd1a3bc10b68823713fdc2b4129
Richard Couzens <richardcouzens96@gmail.com>
2024-07-22 10:20:57 +0100

    added cors

--- part9/patientor/patientorBackend/src/services/patientsService.ts
+++ part9/patientor/patientorBackend/src/services/patientsService.ts
@@ -1,7 +1,18 @@
 import patientsData from "../../data/patients";
+import { NonSensitivePatient } from "../types";
 
 const getPatients = () => {
   return patientsData;
 };
 
-export default { getPatients };
+const getNonSensitivePatients = (): NonSensitivePatient[] => {
+  return patientsData.map(({ id, name, dateOfBirth, gender, occupation }) => ({
+    id,
+    name,
+    dateOfBirth,
+    gender,
+    occupation,
+  }));
+};
+
+export default { getPatients, getNonSensitivePatients };
