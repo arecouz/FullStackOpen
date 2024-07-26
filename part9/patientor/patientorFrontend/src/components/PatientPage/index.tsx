1f511f13d5d8896a3f2346e608a93e9051db5cb7
arecouz <richardcouzens96@gmail.com>
2024-07-26 16:55:31 +0100

    9.24: Patientor, step 5

--- part9/patientor/patientorFrontend/src/components/PatientPage/index.tsx
+++ part9/patientor/patientorFrontend/src/components/PatientPage/index.tsx
@@ -1,21 +1,29 @@
 import {
   Box,
   Table,
-  TableHead,
   Typography,
   TableCell,
   TableRow,
   TableBody,
-} from "@mui/material";
-import patientServices from "../../services/patients";
-import { useParams } from "react-router-dom";
-import { useEffect, useState } from "react";
-import { Patient } from "../../types";
+  Card,
+  CardContent,
+  CardHeader,
+} from '@mui/material';
+import patientServices from '../../services/patients';
+import diagnosisServices from '../../services/diagnosis';
+import { useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { Diagnosis, Entry, Patient } from '../../types';
 
 const PatientPage = () => {
   const [patient, setPatient] = useState<Patient | null>(null);
+  const [diagnosisList, setDiagnosisList] = useState<Diagnosis[]>([]);
 
   const { id } = useParams();
+
+  useEffect(() => {
+    diagnosisServices.getAll().then((response) => setDiagnosisList(response));
+  }, []);
 
   useEffect(() => {
     if (id) {
@@ -27,21 +35,19 @@ const PatientPage = () => {
     return <></>;
   }
 
+  const getDiagnosisDescription = (code: string): string | undefined => {
+    const diagnosis = diagnosisList.find((d) => d.code === code);
+    return diagnosis ? diagnosis.name : undefined;
+  };
+
   return (
     <div>
-      {" "}
       <Box>
-        <Typography align='center' variant='h6'>
+        <Typography align="center" variant="h6">
           {patient.name}
         </Typography>
       </Box>
-      <Table style={{ marginBottom: "1em" }}>
-        <TableHead>
-          <TableRow>
-            <TableCell>Label</TableCell>
-            <TableCell>Details</TableCell>
-          </TableRow>
-        </TableHead>
+      <Table style={{ marginBottom: '1em' }}>
         <TableBody>
           <TableRow>
             <TableCell>Name</TableCell>
@@ -63,13 +69,44 @@ const PatientPage = () => {
             <TableCell>Occupation</TableCell>
             <TableCell>{patient.occupation}</TableCell>
           </TableRow>
-          <TableRow>
-            <TableCell>ID</TableCell>
-            <TableCell>{patient.id}</TableCell>
-          </TableRow>
           {/* Add more rows as necessary */}
         </TableBody>
       </Table>
+      <br />
+      {patient.entries.length !== 0 && (
+        <>
+          {Object.values(patient.entries).map((entry: Entry) => (
+            <Card key={entry.id} style={{ marginBottom: '1em' }}>
+              <CardHeader title={`${entry.date}`} align="center" />
+              <CardContent>
+                <Table>
+                  <TableBody>
+                    <TableRow>
+                      <TableCell>{entry.description}</TableCell>
+                    </TableRow>
+                  </TableBody>
+                </Table>
+                {entry.diagnosisCodes && (
+                  <Table>
+                    <TableBody>
+                      {Object.values(entry.diagnosisCodes).map(
+                        (code: string) => (
+                          <TableRow key={code}>
+                            <TableCell>{code}</TableCell>
+                            <TableCell align="center">
+                              {getDiagnosisDescription(code)}
+                            </TableCell>
+                          </TableRow>
+                        )
+                      )}
+                    </TableBody>
+                  </Table>
+                )}
+              </CardContent>
+            </Card>
+          ))}
+        </>
+      )}
     </div>
   );
 };
