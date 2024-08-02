6b9211dcb629b9bf4ce5e753a2c1e30df3139b52
Richard Couzens <richardcouzens96@gmail.com>
2024-08-02 20:18:56 +0100

    Getting repo's from api (Native React)

--- part10/rate-repository-app/src/components/Bmi.jsx
+++ part10/rate-repository-app/src/components/Bmi.jsx
@@ -1,51 +1,36 @@
-import { Text, TextInput, Pressable, View } from 'react-native';
-import { useFormik } from 'formik';
+import { useState, useEffect } from "react";
+import RepositoryItem from "./RepositoryItem";
+import { FlatList } from 'react-native';
 
-const initialValues = {
-  mass: '',
-  height: '',
-};
 
-const getBodyMassIndex = (mass, height) => {
-  return Math.round(mass / Math.pow(height, 2));
-};
+const RepositoryList = () => {
+  const [repositories, setRepositories] = useState();
 
-const BodyMassIndexForm = ({ onSubmit }) => {
-  const formik = useFormik({
-    initialValues,
-    onSubmit,
-  });
+  const fetchRepositories = async () => {
+    // Replace the IP address part with your own IP address!
+    const response = await fetch("http://192.168.1.197:5000/api/repositories");
+    const json = await response.json();
+
+    console.log(json);
+
+    setRepositories(json);
+  };
+
+  useEffect(() => {
+    fetchRepositories();
+  }, []);
+
+  // Get the nodes from the edges array
+  const repositoryNodes = repositories
+    ? repositories.edges.map((edge) => edge.node)
+    : [];
 
   return (
-    <View>
-      <TextInput
-        placeholder="Weight (kg)"
-        value={formik.values.mass}
-        onChangeText={formik.handleChange('mass')}
+      <FlatList
+        data={repositoryNodes}
+        renderItem={({ item }) => <RepositoryItem item={item} />}
       />
-      <TextInput
-        placeholder="Height (m)"
-        value={formik.values.height}
-        onChangeText={formik.handleChange('height')}
-      />
-      <Pressable onPress={formik.handleSubmit}>
-        <Text>Calculate</Text>
-      </Pressable>
-    </View>
   );
 };
 
-const Bmi = () => {
-  const onSubmit = values => {
-    const mass = parseFloat(values.mass);
-    const height = parseFloat(values.height);
-
-    if (!isNaN(mass) && !isNaN(height) && height !== 0) {
-      console.log(`Your body mass index is: ${getBodyMassIndex(mass, height)}`);
-    }
-  };
-
-  return <BodyMassIndexForm onSubmit={onSubmit} />;
-};
-
-export default Bmi;
+export default RepositoryList;
